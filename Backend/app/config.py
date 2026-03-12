f5945dfa5a52436283bfeb0c08ebb0ef8effda2c
Diego Muciño <dvan0muc897@gmail.com>
2026-03-11 23:53:20 -0600

    Delete Backend directory

--- Backend/app/config.py
+++
@@ -1,9 +0,0 @@
-class Config:
-
-    SECRET_KEY = "12345"
-
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@host.docker.internal/flask_auth_api"
-
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    JWT_SECRET_KEY = "67890"
